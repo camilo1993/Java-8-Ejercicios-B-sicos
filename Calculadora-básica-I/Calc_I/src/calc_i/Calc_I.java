de38026de49f4c6af16217cee04d6762b9841b05
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-04-07 14:07:02 -0300

    Basic Calculator

---
+++ Calculadora-básica-I/Calc_I/src/calc_i/Calc_I.java
@@ -0,0 +1,99 @@
+
+
+package calc_i;
+
+
+public class Calc_I {
+
+
+    public static void main(String[] args) {
+
+    char x;
+    
+    x='c';
+    
+    
+    switch (x)
+    {
+        case 'a':  
+    {
+        
+       System.out.println("Suma");
+        
+       double a=3;
+       
+       double b=4;
+       
+       double c=a+b;
+       
+     
+       System.out.println("El resultado es " + (c));
+       break;
+       
+    }        
+    case 'b':
+    {
+     
+    System.out.println("Resta");
+        
+       double a=65;
+       
+       double b=4;
+       
+       double c=a-b;
+       
+     
+       System.out.println("El resultado es " + (c));    
+       break;
+        
+        
+        
+    }
+    case 'c':
+    {
+        System.out.println("Multiplicación");
+        
+       double a=65;
+       
+       double b=4;
+       
+       double c=a*b;
+       
+     
+       System.out.println("El resultado es " + (c));  
+       break;
+        
+    }    
+    case 'd':
+    {
+        System.out.println("división");
+        
+       double a=65;
+       
+       double b=4;
+       
+       double c=a/b;
+       
+     
+       System.out.println("El resultado es " + (c)); 
+       break;
+        
+    }    
+        
+    }   
+    
+    
+        
+   
+        
+        
+
+    
+    
+
+        
+ 
+
+    }
+    
+}
