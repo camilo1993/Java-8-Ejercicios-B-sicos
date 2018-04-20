8e67106d9d6eecf274157d31c3fbfd28830d4273
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-04-20 17:50:50 -0300

    Basic Calculator II

--- Calculadora-básica-II/CalcII/src/calcii/CalcII.java
+++ Calculadora-básica-II/CalcII/src/calcii/CalcII.java
@@ -5,7 +5,7 @@
  */
 package calcii;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 /**
  *
@@ -17,25 +17,36 @@ public class CalcII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-                String [] arc=new String[5];
-        
-        for(int i=0;i<5;i++){
-            
-            arc[i]=JOptionPane.showInputDialog("Introducir Números " + (i+1));
-        }
-        
 
-        
-        for(String elemento: arc){
-    
-            System.out.println("Número:" + elemento);
+        Scanner reader = new Scanner(System.in);
+        float matriz[] = new float[5];
+        float num2;
+        char operacion;
+        float resultado;
+
+        do {
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.println("Ingresar Numeros");
+                matriz[i] = reader.nextInt();
+            }
+        } while (matriz[0] > 100 || matriz[0] < 0);
+
+        do {
+            System.out.println("Digite Segundo Número");
+            num2 = reader.nextFloat();
+        } while (num2 > 100 || num2 < 0);
+
+        System.out.println("Digite Operación (+-/*) ");
+        operacion = reader.next().charAt(0);
+
+        switch (operacion) {
+
+            case '+':
+
+                resultado = matriz[0] + num2;
+                System.out.println("El resultado es = " + resultado);
+
         }
+
     }
-    
 }
-        
-        
-        
-        
-    
